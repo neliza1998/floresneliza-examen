ec74da3cbbfb89a940c91b3c69f08409e44ac521
yuliñotuyo <martinezlinoestela@gmail.com>
2016-03-17 17:08:41 -0500

    neliza

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>neliza flores pillaca</title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+</head>
+<body>
+<div class="container">
+	
+
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+
+			</button>
+			<a class="navbar-brand" href="#">Neliza</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Link</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</nav>
+<center>
+	
+
+<div id="carousel-id" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-id" data-slide-to="0" class="active"></li>
+		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="2" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="3" class=""></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="item">
+			<img data-src="public/imagenes/neli.jpg" alt="First slide" src="public/imagenes/neli.jpg" width="380">
+			<div class="container">
+				<div class="carousel-caption">
+					
+				</div>
+			</div>
+		</div>
+		<div class="item active">
+			<img data-src="public/imagenes/nely.jpg" alt="Third slide" src="public/imagenes/nely.jpg" width="500">
+			<div class="container">
+				<div class="carousel-caption">
+					
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<img data-src="public/imagenes/neliza.jpg" alt="Second slide" src="public/imagenes/neliza.jpg" width="380">
+			<div class="container">
+				<div class="carousel-caption">
+				
+				</div>
+			</div>
+		</div>
+		<div class="item ">
+			<img data-src="public/imagenes/nely.jpg" alt="five slide" src="public/imagenes/nely.jpg" width="500">
+			<div class="container">
+				<div class="carousel-caption">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+</center>
+<script src="public/js/jquery-1.12.1.js"></script>
+<script src="public/js/bootstrap.js"></script>	
+</div>
+</body>
+</html>
+	
+	
